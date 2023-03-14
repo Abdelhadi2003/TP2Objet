44ff78598e2204e0eff1d520b790e0b8cc26fd12
Abdelhadi Mejdoubi <2050342@cmontmorency.qc.ca>
2023-03-14 01:56:08 -0400

    J'ai fais la fonction afficherPartie pour les information lié à la partie et la fonction affichage_regles

--- jeuSequence.py
+++ jeuSequence.py
@@ -10,6 +10,7 @@ class Partie():
         self.nomJoueur = ""
         self.pointage = 0
         self.sequence_en_cours = []
+        self.sequence_joueur = []
         self.niveau_pause = 0.5
         self.partie_en_cours = False
         self.led_bleu = Pin(13, Pin.OUT)
@@ -19,4 +20,16 @@ class Partie():
         self.btn_rouge = Pin(17, Pin.IN, Pin.PULL_UP)
         self.btn_vert = Pin(18, Pin.IN, Pin.PULL_UP)
     
-    
+    def afficherPartie(self):
+        print("Votre nom : "+ self.nomJoueur)
+        print("Votre pointage : " + str(self.pointage) + " points")
+             
+    def affichage_regles(self):
+        print("|-----------------------------------------------------------------|")
+        print("|    Travail pratique 2 - Jeux séquence - Abdelhadi Mejdoubi           |")
+        print("|-----------------------------------------------------------------|")
+        print("| Le principe du jeu est de reproduire une séquence de lumières LED donnée par l’ordinateur. |")
+        print("| Quand la séquence est réussie par le joueur, une nouvelle séquence de lumières LED est donnée par l’ordinateur en ajoutant une lumière LED de plus aléatoirement.|")
+        print("| Et ainsi de suite jusqu’à ce que le joueur atteigne le maximum de 20 séquences ou que je joueur se trompe dans la séquence.|")          
+        print("|-----------------------------------------------------------------|")
+        print("")
